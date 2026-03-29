7da0d2298e8cb3fcc48bff03c583fba0644e20e0
Fernando Siller <Fersill53@outlook.com>
2026-03-29 12:39:30 -0500

    first commit of generated files

---
+++ src/app/shared/recipe-carousel/recipe-carousel.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeCarousel } from './recipe-carousel';
+
+describe('RecipeCarousel', () => {
+  let component: RecipeCarousel;
+  let fixture: ComponentFixture<RecipeCarousel>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RecipeCarousel],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RecipeCarousel);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/recipe-carousel/recipe-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-carousel',
+  imports: [],
+  templateUrl: './recipe-carousel.html',
+  styleUrl: './recipe-carousel.scss',
+})
+export class RecipeCarousel {}
