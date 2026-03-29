e139921dfeb0ba6c0f4a5d6c4cc266025a93743f
Fernando Siller <Fersill53@outlook.com>
2026-03-29 18:59:25 -0500

    bottom nav, favs, recipes, planner, comps inst

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,33 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { PlannerComponent } from './pages/planner/planner.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+
+    {
+        path: 'recipes',
+        component: RecipesComponent
+    },
+
+    {
+        path: 'planner',
+        component: PlannerComponent
+    },
+
+    {
+        path: 'favorites',
+        component: FavoritesComponent
+    },
+
+
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
