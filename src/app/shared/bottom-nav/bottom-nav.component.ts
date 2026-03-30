afb032d94d578441f038c9bec167be2295fdd907
Fernando Siller <Fersill53@outlook.com>
2026-03-29 19:22:27 -0500

    fix for bottomnav

--- src/app/shared/bottom-nav/bottom-nav.component.ts
+++ src/app/shared/bottom-nav/bottom-nav.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-bottom-nav',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, MatIconModule],
+  imports: [NgFor, RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './bottom-nav.component.html',
-  styleUrl: './bottom-nav.component.scss',
+  styleUrl: './bottom-nav.component.scss'
 })
 export class BottomNavComponent {
   navItems = [
     { label: 'Home', icon: 'home', route: '/' },
     { label: 'Recipes', icon: 'restaurant_menu', route: '/recipes' },
     { label: 'Planner', icon: 'calendar_month', route: '/planner' },
-    { label: 'Favorites', icon: 'favorite', route: '/favorites' },
+    { label: 'Favorites', icon: 'favorite', route: '/favorites' }
   ];
 }
