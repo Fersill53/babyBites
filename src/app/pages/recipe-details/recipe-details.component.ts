d7e1ad83c8cbd5c8607f05b23886518150e45ab5
Fernando Siller <Fersill53@outlook.com>
2026-03-29 21:57:43 -0500

    recipe details forgot to add import

--- src/app/pages/recipe-details/recipe-details.component.ts
+++ src/app/pages/recipe-details/recipe-details.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { RECIPES, Recipe } from '../../data/recipes.data';
 
 @Component({
   selector: 'app-recipe-details',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatIconModule, HeaderComponent],
   templateUrl: './recipe-details.component.html',
-  styleUrl: './recipe-details.component.scss',
+  styleUrl: './recipe-details.component.scss'
 })
-export class RecipeDetailsComponent {}
+export class RecipeDetailsComponent {
+  recipe?: Recipe;
+
+  constructor(private route: ActivatedRoute) {
+    const slug = this.route.snapshot.paramMap.get('slug');
+    this.recipe = RECIPES.find((item) => item.slug === slug);
+  }
+}
